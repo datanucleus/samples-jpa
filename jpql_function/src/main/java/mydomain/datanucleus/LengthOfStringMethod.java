0f19c6f284da384101532f9f989e0fd3b66c6709
andyjefferson <andy@datanucleus.org>
2018-02-10 13:31:34 +0000

    bring up to date

--- jpql_function/src/main/java/mydomain/datanucleus/LengthOfStringMethod.java
+++ jpql_function/src/main/java/mydomain/datanucleus/LengthOfStringMethod.java
@@ -4,38 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.datanucleus.exceptions.NucleusUserException;
+import org.datanucleus.store.rdbms.sql.SQLStatement;
 import org.datanucleus.store.rdbms.sql.expression.NumericExpression;
 import org.datanucleus.store.rdbms.sql.expression.SQLExpression;
 import org.datanucleus.store.rdbms.sql.expression.StringExpression;
-import org.datanucleus.store.rdbms.sql.method.AbstractSQLMethod;
+import org.datanucleus.store.rdbms.sql.method.SQLMethod;
 
 /**
  * Expression handler to evaluate LENGTHOFSTRING({expression}).
  * Returns a NumericExpression.
  */
-public class LengthOfStringMethod extends AbstractSQLMethod
+public class LengthOfStringMethod implements SQLMethod
 {
-    public SQLExpression getExpression(SQLExpression ignore, List args)
+    public SQLExpression getExpression(SQLStatement stmt, SQLExpression expr, List<SQLExpression> args)
     {
         if (args == null || args.size() == 0)
         {
             throw new NucleusUserException("Cannot invoke LENGTHOFSTRING without an argument");
         }
 
-        SQLExpression expr = (SQLExpression)args.get(0);
-        if (expr == null)
+        SQLExpression arg0Expr = (SQLExpression)args.get(0);
+        if (arg0Expr == null)
         {
             throw new NucleusUserException("Cannot invoke LENGTHOFSTRING with null argument");
         }
-        if (expr instanceof StringExpression)
+        if (arg0Expr instanceof StringExpression)
         {
             ArrayList funcArgs = new ArrayList();
-            funcArgs.add(expr);
-            return new NumericExpression(stmt, getMappingForClass(int.class), "CHAR_LENGTH", funcArgs);
+            funcArgs.add(arg0Expr);
+            return new NumericExpression(stmt, stmt.getSQLExpressionFactory().getMappingForType(int.class, true), "CHAR_LENGTH", funcArgs);
         }
         else
         {
-            throw new NucleusUserException("Argument for LENGTHOFSTRING has to be a StringExpression/StringLiteral - invalid to pass " + expr);
+            throw new NucleusUserException("Argument for LENGTHOFSTRING has to be a StringExpression/StringLiteral - invalid to pass " + arg0Expr);
         }
     }
 }
